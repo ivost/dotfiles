3ec192313ed145a17e9ad74f40b3c2a885434142
Ivo Stoyanov <ivostoy@gmail.com>
2019-06-15 14:04:06 -0700

    w

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+
 cd ~
-ln -s ~/dotfiles/.bash_aliases .bash_aliases
-ln -s ~/dotfiles/.vimrc .vimrc
+
+[[ ! -f .bash_aliases ]] && ln -s ~/dotfiles/.bash_aliases .bash_aliases
+[[ ! -f .vimrc ]] && ln -s ~/dotfiles/.vimrc .vimrc
+
+git config --global user.email "ivostoy@gmail.com"
+git config --global user.name "Ivo Stoyanov"
+
  
